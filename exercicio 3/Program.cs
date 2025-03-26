cf7c96a0c8b7e3352fb4fcc60126131d27d24d47
GabriellllllllllB <164575563+GabriellllllllllB@users.noreply.github.com>
2025-03-26 20:49:40 -0300

    Add files via upload

---
+++ exercicio 3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Produto produto = new Produto();
+
+        Console.Write("escreva o produto: ");
+        produto.nome = Console.ReadLine();
+        Console.Write("escreva o preço: ");
+        produto.preço = Convert.ToDouble(Console.ReadLine());
+        Console.Write("escreva a quantidade do estoque: ");
+        produto.quantidade = int.Parse(Console.ReadLine());
+
+        while (true)
+        {
+            Console.WriteLine("escolha uma opcao:");
+            Console.WriteLine("1 adicionar ao estoque");
+            Console.WriteLine("2 remover do estoque");
+            Console.WriteLine("3 exibir detalhes");
+            int opcao = int.Parse(Console.ReadLine());
+
+            if (opcao == 1)
+            {
+                Console.Write("escreva a quantidade que você deseja adicionar ao estoque: ");
+                int quantidade = int.Parse(Console.ReadLine());
+                produto.adicionarestoque(quantidade);
+            }
+            else if (opcao == 2)
+            {
+                Console.Write("escreva a quantidade que você deseja remover do estoque: ");
+                int quantidade = int.Parse(Console.ReadLine());
+                produto.removerestoque(quantidade);
+            }
+            else if (opcao == 3)
+            {
+                produto.exibirdetalhes();
+            }
+        }
+    }
+}
