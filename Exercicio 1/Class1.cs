21aed71d3fdc22c0b6e075c58b5dfeb016191c72
GabriellllllllllB <164575563+GabriellllllllllB@users.noreply.github.com>
2025-03-26 17:56:37 -0300

    Add files via upload

---
+++ Exercicio 1/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Aluno
+{
+    public string ra;
+    public string nome;
+    public double notaprova;
+    public double notatrabalho;
+    public double notafinal;
+
+    public void dados()
+    {
+
+        Console.WriteLine("escreva o RA do aluno");
+        ra = Console.ReadLine();
+        Console.WriteLine("escreva o nome do aluno");
+        nome = Console.ReadLine();
+        Console.WriteLine("escreva o valor da nota da prova");
+        notaprova = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("escreva o valor da nota do trabalho");
+        notatrabalho = Convert.ToDouble(Console.ReadLine());
+
+    }
+    public void media()
+    {
+        notafinal = (notaprova + notatrabalho) / 2;
+    }
+    public void notaf()
+    {
+        if (notafinal >= 6.0)
+        {
+            Console.WriteLine($" {nome} {ra} aprovado, média {notafinal:F2}.");
+        }
+        else
+        {
+            double notamedia = 12;
+            notamedia = 12 - notafinal;
+            Console.WriteLine($"aluno {nome} (ra: {ra}) precisa de {notamedia:F2} na prova final.");
+        }
+    }
+}
+
+
+
+
---
+++ exercicio 2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Contabancaria conta = new Contabancaria();
+
+            
+        int opcao = 0;
+
+        while (opcao != 4) 
+        {
+
+
+            Console.WriteLine("menu de opções:");
+            Console.WriteLine("1 - depositar");
+            Console.WriteLine("2 - sacar");
+            Console.WriteLine("3 - exibir saldo");
+
+            Console.Write("escolha uma opção: ");
+            opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    Console.Write("escreva o valor do deposito: R$ ");
+                    double valorDeposito = double.Parse(Console.ReadLine());
+                    conta.Depositar(valorDeposito);
+                    break;
+                    
+                case 2:
+                    Console.Write("escreva o valor do saque: R$ ");
+                    double valorSaque = double.Parse(Console.ReadLine());
+                    conta.sacar(valorSaque);
+                    break;
+                case 3:
+                    conta.exibir();
+                    break;
+                default:
+                    Console.WriteLine("tente outra opção");
+                    break;
+
+            }
+
+        } 
+    }
+}
