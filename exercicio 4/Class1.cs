fde32077b9c8039dc89ead434dd38ac20cd6b97e
GabriellllllllllB <164575563+GabriellllllllllB@users.noreply.github.com>
2025-03-26 21:07:21 -0300

    Add files via upload

---
+++ exercicio 4/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+internal class Lampada
+{
+    private bool Ligada;
+
+    public void Ligar()
+    {
+        Ligada = true;
+        Console.WriteLine("a lampada foi ligada.");
+    }
+
+    public void Desligar()
+    {
+        Ligada = false;
+        Console.WriteLine("a lampada foi desligada.");
+    }
+
+    public void MostrarEstado()
+    {
+        if (Ligada)
+            Console.WriteLine("a lampada esta ligada.");
+        else
+            Console.WriteLine("a lampada esta desligada.");
+    }
+}
---
+++ exercicio 4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Lampada lampada = new Lampada();
+        int opcao;
+
+        do
+        {
+            Console.WriteLine("menu da lampada:");
+            Console.WriteLine("1 ligar a lampada");
+            Console.WriteLine("2 desligar a lampada");
+            Console.WriteLine("3 mostrar estado da lampada");
+            Console.Write("escolha uma opção: ");
+
+            if (int.TryParse(Console.ReadLine(), out opcao))
+            {
+                switch (opcao)
+                {
+                    case 1:
+                        lampada.Ligar();
+                        break;
+                    case 2:
+                        lampada.Desligar();
+                        break;
+                    case 3:
+                        lampada.MostrarEstado();
+                        break;
+                }
+            
+            }
+        } while (opcao != 0);
+    }
+}
